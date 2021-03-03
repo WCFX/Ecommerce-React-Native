456f5f00970ccdcd1fcc8034601b5074152beac6
wcfx <wcfx.dev@gmail.com>
2021-03-03 16:49:30 -0300

    Fonte Poppins, Archivo e Nunito adicionadas ao projeto

---
+++ src/views/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #eee;
+`;
+
+export const Hello = styled.Text`
+  font-size: 22px;
+`;
